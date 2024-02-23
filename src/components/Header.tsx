3fab17fdf5ac8f4ddcb05a14f6aec29823b6e4bf
Erick Cuevas <ecuevasceballos@gmail.com>
2024-02-22 19:55:36 -0600

    third commit

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -6,7 +6,7 @@ const Header = () => {
       <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 flex flex-col justify-center items-center max-w-2xl px-5 h-screen hover:scale-105 hover:duration-200 cursor-pointer">
         <img src={logo} alt="Rick & Morty logo" />
         <h1 className="uppercase font-bold tracking-widest text-2xl sm:text-4xl max-w-2xl text-center text-gray-200">
-          Characters
+          Character
         </h1>
       </div>
     </header>
